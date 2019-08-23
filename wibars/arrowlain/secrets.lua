a77a20faf91c24aaab672bb3708a8d7f35e839aa
Kirill Bugaev <kirill.bugaev87@gmail.com>
2019-08-23 17:32:10 +0800

    some fixes

---
+++ wibars/arrowlain/secrets.lua
@@ -0,0 +1,18 @@
+local secrets = {
+
+	-- OpenWeatherMap API key - https://openweathermap.org/appid
+	openweather_api_key = "3e321f9414eaedbfab34983bda77a66e",
+
+	-- Mail accounts
+	mail1 = {
+		account  = "mai11@gmail.com",
+		password = "password"
+	},
+	mail2 = {
+		account  = "mail2@gmail.com",
+		password = "password"
+	}
+
+}
+
+return secrets
